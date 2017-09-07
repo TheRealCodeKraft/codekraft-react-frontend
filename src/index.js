1445acf7dca6fb576a7f52c021dcd8a7ce12f812
Arnaud DRAZEK <adrazek@gmail.com>
2017-09-07 15:29:37 +0000

    Removing old beginning code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-import React from "react"
 import _createStore from './api/client/reducer/create-store'
-
-class Index extends React.Component {
-
-  render() {
-    return (
-      <span>PAF POUF PUF</span>
-    )
-  }
-
-}
-
-exports.Index = Index
 exports.createStore = _createStore
