7fb50b0b7659a297faca118f4d771a319d58d73b
Arnaud DRAZEK <adrazek@gmail.com>
2017-09-07 15:08:11 +0000

    IE FIX git status

--- dist-modules/api/client/reducer/base-reducers.js
+++ dist-modules/api/client/reducer/base-reducers.js
@@ -4,13 +4,27 @@ var _authReducer = function _authReducer() {
   var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
   var action = arguments[1];
 
+  var newState = {};
   switch (action.type) {
     case "TOKEN":
-      return Object.assign({}, state, { token: action.token });
+      newState["token"] = action.token;
+      break;
     default:
       break;
   }
-  return state;
+
+  var keys = Object.keys(state);
+  if (keys.length > 0) {
+    //return Object.assign({}, state, newState)
+    for (var key in keys) {
+      key = keys[key];
+      if (newState[key] === undefined) {
+        newState[key] = state[key];
+      }
+    }
+  }
+
+  return newState;
 };
 
 // The User Reducer
@@ -18,19 +32,44 @@ var _userReducer = function _userReducer() {
   var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
   var action = arguments[1];
 
+  var newState = {};
   switch (action.type) {
     case "USERS":
-      return Object.assign({}, state, { users: action.users });
+      newState["users"] = action.users;
+      break;
     case 'RESET_ME':
-      return Object.assign({}, state, { me: null, authenticated: false, notFound: false });
+      newState["me"] = null;
+      newState["authenticated"] = false;
+      newState["notFound"] = false;
+      break;
     case 'ME':
-      return Object.assign({}, state, { me: action.user, authenticated: true, notFound: false });
+      console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+      newState["me"] = action.user;
+      newState["authenticated"] = true;
+      newState["notFound"] = false;
+      break;
     case 'USER_NOT_FOUND':
-      return Object.assign({}, state, { me: null, authenticated: false, notFound: true });
+      newState["me"] = null;
+      newState["authenticated"] = false;
+      newState["notFound"] = true;
+      break;
     default:
       break;
   }
-  return state;
+
+  var keys = Object.keys(state);
+  if (keys.length > 0) {
+    return Object.assign({}, state, newState);
+    for (var key in keys) {
+      key = keys[key];
+      if (newState[key] === undefined) {
+        newState[key] = state[key];
+      }
+    }
+  }
+  console.dir(newState);
+
+  return newState;
 };
 
 exports.authReducer = _authReducer;
