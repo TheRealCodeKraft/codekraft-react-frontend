20008b4cff0a1f058e0ced8a4ab42cfacd59e988
Arnaud DRAZEK <adrazek@gmail.com>
2017-10-03 13:50:12 +0000

    Adding page managment

---
+++ src/config/navigation/admin/page.js
@@ -0,0 +1,42 @@
+const config = {
+  route: "pages",
+  title: "Contenus",
+  description: "Accédez à la liste des pages de contenu",
+  client: "PageClient",
+  icon: "pages",
+  list: {
+    attributes: [
+      {
+        name: "id",
+        hidden: true
+      },
+      {
+        name: "title",
+        label: "Titre"
+      }
+    ]
+  },
+  form: {
+    attributes: [
+      {
+        name: "title",
+        label: "Titre",
+        placeholder: "Titre de la page",
+        type: "text",
+        required: true
+      },
+      {
+        name: "body",
+        label: "Contenu",
+        placeholder: "Corps du contenu",
+        type: "textarea"
+      }
+    ]
+  },
+  delete: {
+    labels: {
+      entity: "la page",
+      identifier: "title"
+    }
+  }
+} 
