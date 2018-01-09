4b3522e691cc6152fa80d31df5dbb77117236443
nO <arnaud@codekraft.fr>
2018-01-09 21:25:47 +0100

    Managing static toolbar on wysiwyg

--- dist-modules/components/utils/form/wysiwyg.js
+++ dist-modules/components/utils/form/wysiwyg.js
@@ -4,13 +4,25 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
+var _draftJsPluginsEditor = require("draft-js-plugins-editor");
+
+var _draftJsPluginsEditor2 = _interopRequireDefault(_draftJsPluginsEditor);
+
 var _draftJs = require("draft-js");
+
+var _draftJsStaticToolbarPlugin = require("draft-js-static-toolbar-plugin");
+
+var _draftJsStaticToolbarPlugin2 = _interopRequireDefault(_draftJsStaticToolbarPlugin);
+
+var _draftJsButtons = require("draft-js-buttons");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -20,20 +32,127 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var HeadlinesPicker = function (_Component) {
+  _inherits(HeadlinesPicker, _Component);
+
+  function HeadlinesPicker() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, HeadlinesPicker);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = HeadlinesPicker.__proto__ || Object.getPrototypeOf(HeadlinesPicker)).call.apply(_ref, [this].concat(args))), _this), _this.onWindowClick = function () {
+      return (
+        // Call `onOverrideContent` again with `undefined`
+        // so the toolbar can show its regular content again.
+        _this.props.onOverrideContent(undefined)
+      );
+    }, _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  _createClass(HeadlinesPicker, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      setTimeout(function () {
+        window.addEventListener('click', _this2.onWindowClick);
+      });
+    }
+  }, {
+    key: "componentWillUnmount",
+    value: function componentWillUnmount() {
+      window.removeEventListener('click', this.onWindowClick);
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this3 = this;
+
+      var buttons = [_draftJsButtons.HeadlineOneButton, _draftJsButtons.HeadlineTwoButton, _draftJsButtons.HeadlineThreeButton];
+      return _react2.default.createElement(
+        "div",
+        null,
+        buttons.map(function (Button, i) {
+          return (// eslint-disable-next-line
+            _react2.default.createElement(Button, _extends({ key: i }, _this3.props))
+          );
+        })
+      );
+    }
+  }]);
+
+  return HeadlinesPicker;
+}(Component);
+
+var HeadlinesButton = function (_Component2) {
+  _inherits(HeadlinesButton, _Component2);
+
+  function HeadlinesButton() {
+    var _ref2;
+
+    var _temp2, _this4, _ret2;
+
+    _classCallCheck(this, HeadlinesButton);
+
+    for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      args[_key2] = arguments[_key2];
+    }
+
+    return _ret2 = (_temp2 = (_this4 = _possibleConstructorReturn(this, (_ref2 = HeadlinesButton.__proto__ || Object.getPrototypeOf(HeadlinesButton)).call.apply(_ref2, [this].concat(args))), _this4), _this4.onClick = function () {
+      return (
+        // A button can call `onOverrideContent` to replace the content
+        // of the toolbar. This can be useful for displaying sub
+        // menus or requesting additional information from the user.
+        _this4.props.onOverrideContent(HeadlinesPicker)
+      );
+    }, _temp2), _possibleConstructorReturn(_this4, _ret2);
+  }
+
+  _createClass(HeadlinesButton, [{
+    key: "render",
+    value: function render() {
+      return _react2.default.createElement(
+        "div",
+        { className: editorStyles.headlineButtonWrapper },
+        _react2.default.createElement(
+          "button",
+          { onClick: this.onClick, className: editorStyles.headlineButton },
+          "H"
+        )
+      );
+    }
+  }]);
+
+  return HeadlinesButton;
+}(Component);
+
+var toolbarPlugin = (0, _draftJsStaticToolbarPlugin2.default)({
+  structure: [_draftJsButtons.BoldButton, _draftJsButtons.ItalicButton, _draftJsButtons.UnderlineButton, _draftJsButtons.CodeButton, _draftJsStaticToolbarPlugin.Separator, HeadlinesButton, _draftJsButtons.UnorderedListButton, _draftJsButtons.OrderedListButton, _draftJsButtons.BlockquoteButton, _draftJsButtons.CodeBlockButton]
+});
+var Toolbar = toolbarPlugin.Toolbar;
+
+var plugins = [toolbarPlugin];
+
 var Wysiwyg = function (_React$Component) {
   _inherits(Wysiwyg, _React$Component);
 
   function Wysiwyg(props) {
     _classCallCheck(this, Wysiwyg);
 
-    var _this = _possibleConstructorReturn(this, (Wysiwyg.__proto__ || Object.getPrototypeOf(Wysiwyg)).call(this, props));
+    var _this5 = _possibleConstructorReturn(this, (Wysiwyg.__proto__ || Object.getPrototypeOf(Wysiwyg)).call(this, props));
 
-    _this.state = {
+    _this5.state = {
       loaded: false,
       raw: "",
       editorState: _draftJs.EditorState.createEmpty()
     };
-    return _this;
+    return _this5;
   }
 
   _createClass(Wysiwyg, [{
@@ -106,7 +225,7 @@ var Wysiwyg = function (_React$Component) {
             _react2.default.createElement("i", { className: "fa fa-list-ol" })
           )
         ),
-        _react2.default.createElement(_draftJs.Editor, { editorState: this.state.editorState, onChange: this.onChange.bind(this) })
+        _react2.default.createElement(_draftJsPluginsEditor2.default, { editorState: this.state.editorState, onChange: this.onChange.bind(this), plugins: plugins })
       );
     }
   }, {
--- src/components/utils/form/wysiwyg.js
+++ src/components/utils/form/wysiwyg.js
@@ -1,6 +1,83 @@
 import React from "react"
 
-import {Draft, Editor, EditorState, ContentState, RichUtils, convertFromRaw} from 'draft-js';
+import Editor from "draft-js-plugins-editor"
+import {Draft, EditorState, ContentState, RichUtils, convertFromRaw} from 'draft-js';
+
+import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+
+class HeadlinesPicker extends Component {
+  componentDidMount() {
+    setTimeout(() => { window.addEventListener('click', this.onWindowClick); });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
+  }
+
+  onWindowClick = () =>
+    // Call `onOverrideContent` again with `undefined`
+    // so the toolbar can show its regular content again.
+    this.props.onOverrideContent(undefined);
+
+  render() {
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+    return (
+      <div>
+        {buttons.map((Button, i) => // eslint-disable-next-line
+          <Button key={i} {...this.props} />
+        )}
+      </div>
+    );
+  }
+}
+
+class HeadlinesButton extends Component {
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
+
+  render() {
+    return (
+      <div className={editorStyles.headlineButtonWrapper}>
+        <button onClick={this.onClick} className={editorStyles.headlineButton}>
+          H
+        </button>
+      </div>
+    );
+  }
+}
+
+const toolbarPlugin = createToolbarPlugin({
+  structure: [
+    BoldButton,
+    ItalicButton,
+    UnderlineButton,
+    CodeButton,
+    Separator,
+    HeadlinesButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton
+  ]
+});
+const { Toolbar } = toolbarPlugin;
+const plugins = [toolbarPlugin];
 
 class Wysiwyg extends React.Component {
   constructor(props) {
@@ -36,7 +113,7 @@ class Wysiwyg extends React.Component {
           <a onClick={this._onUlClick.bind(this)}><i className="fa fa-list" /></a>
           <a onClick={this._onOlClick.bind(this)}><i className="fa fa-list-ol" /></a>
         </div>
-        <Editor editorState={this.state.editorState} onChange={this.onChange.bind(this)} />
+        <Editor editorState={this.state.editorState} onChange={this.onChange.bind(this)} plugins={plugins} />
       </div>
     );
   }
